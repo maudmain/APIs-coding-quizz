2b2b6f4fe728cf8c209545fd0888c654eebc44a4
maudmain <117314424+maudmain@users.noreply.github.com>
2023-01-21 17:21:00 +0000

    answerClick/ transition fucntion added

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,17 @@ let questionContainer = document.querySelector("#questions");
 
 
 // declare variables needed
+let time;
 let timer;
 let timerCount;
+let questionCounter;
 
 questionsDOM = [];
 
 // the startGame function is called when the start button is clicked
 function startGame() {
 
+    questionCounter = -1;
 
     questionsArray.forEach(q => {
         questionsDOM.push({
@@ -24,11 +27,16 @@ function startGame() {
     })
 
     setTimer();
+    transition();
 };
 
 function createQuestionDiv(question) {
     let questionDiv = document.createElement("div");
     let questionTitle = document.createElement("h2");
+
+    //set all questionDiv to class hide
+    questionDiv.className = "question hide";
+
     questionDiv.appendChild(questionTitle);
     questionContainer.appendChild(questionDiv);
     questionTitle.textContent = question.title;
@@ -39,7 +47,7 @@ function createQuestionDiv(question) {
         questionDiv.appendChild(optionDiv);
 
         // create a button for each answer
-        let answerButton =document.createElement ("button");
+        let answerButton = document.createElement("button");
         answerButton.textContent = o.answer;
         optionDiv.appendChild(answerButton)
 
@@ -48,38 +56,78 @@ function createQuestionDiv(question) {
         answerButton.dataset.isCorrect = o.isCorrect ?? false
 
         // add eventListener for the answer button and pass the reference to the function
-        answerButton.addEventListener("click",answerClick );
-      
+        answerButton.addEventListener("click", answerClick);
+
     });
-
-
 
     return questionDiv;
 
 }
 // when the user click an answer
-function answerClick (event){
-    event.target.className = "correct";
+function answerClick(event) {
+    let currentQuestion = questionsDOM[questionCounter];
 
 
-};
+    if (event.target.dataset.isCorrect === 'true') {
+        event.target.className = "correct";
+        currentQuestion.answer = true;
+
+    } else {
+        event.target.className = "incorrect";
+        currentQuestion.answer = false;
+        time -= 10;
+
+
+    }
+    transition();
+}
 
 // set a timer
-function setTimer(){
-    let time = questionsArray.length *10;
-    let timer = setInterval(function(){
+function setTimer() {
+    time = questionsArray.length * 7;
+    let timer = setInterval(function () {
         time--;
         timerElement.textContent = time
 
-        if(time <= 0){
+        if (time <= 0) {
             clearInterval(timer);
             timerElement.textContent = "Time Off. Game over"
+            endGame();
         }
     }, 1000);
 
 }
 
 // add eventListener for startGame when clicking the button (not on page loading)
-startButton.addEventListener("click",()=>{startGame()});
+startButton.addEventListener("click", () => { startGame() });
 
-//
+// transition function
+function transition() {
+    if (questionCounter >= 0) {
+        let currentQuestion = questionsDOM[questionCounter];
+        currentQuestion.element.className = "question hide";
+    }
+    questionCounter++;
+
+    if (questionCounter < questionsDOM.length) {
+        let newQuestion = questionsDOM[questionCounter];
+        newQuestion.element.className = "question"
+    } else {
+        endGame();
+    }
+}
+
+// function endGame
+function endGame() {
+    let score = 0
+    questionsDOM.forEach(q => {
+        q.element.className = "question"
+        if (q.answer === true) {
+            score++;
+        }
+        q.element.querySelectorAll("button").forEach(b => b.removeEventListener("click", answerClick) );
+    });
+    alert("you're score is" + score);
+}
+
+
