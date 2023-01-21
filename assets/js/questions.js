7043adc794b94a6b23fe0079aedaa00776f4638d
maudmain <117314424+maudmain@users.noreply.github.com>
2023-01-21 10:57:31 +0000

    questions array populated

---
+++ assets/js/questions.js
@@ -0,0 +1,114 @@
+const questionArray = [
+
+    {
+        number: 1,
+        question: "What is the correct way to declare a variable in JavaScript?",
+        options: [
+            { answer: "#var myVariable" },
+            { answer: "%myVariable" },
+            { answer: "var myVariable", isCorrect: true },
+            { answer: "&myVariable" },
+
+        ]
+    },
+    {
+        number: 2,
+        question: "Which of the following is not a data type in JavaScript?",
+        optionS: [
+            { answer: "String" },
+            { answer: "Boolean" },
+            { answer: "Array" },
+            { answer: "Integer", isCorrect: true },
+        ]
+    },
+
+    {
+        number: 3,
+        question: "What is the correct way to create an object in JavaScript ?",
+        options: [
+            { answer: "object myObject" },
+            { answer: "myObject = new Object()" },
+            { answer: "var myObject = {}", isCorrect: true },
+            { answer: "myObject = []" },
+
+        ]
+    },
+    {
+        number: 4,
+        question: "Which of the following is not a looping construct in JavaScript ?",
+        options: [
+            { answer: "for" },
+            { answer: "while" },
+            { answer: "do -while" },
+            { answer: "repeat", isCorrect :true },
+
+        ]
+    },
+    {
+        number: 5,
+        question: "What is the correct way to add an element to an array in JavaScript ?",
+        options: [
+            { answer: "myArray.add(element)" },
+            { answer: "myArray.append(element)" },
+            { answer: "myArray[myArray.length] = element" },
+            { answer: "myArray.push(element)", isCorrect: true },
+
+        ]
+    },
+    {
+        number: 6,
+        question: "Which of the following is not an event in JavaScript ?",
+        options: [
+            { answer: "onclick" },
+            { answer: "onsubmit" },
+            { answer: "onload" },
+            { answer: "onhover", isCorrect: true },
+
+        ]
+    },
+    {
+        number: 7,
+        question: "What is the correct way to create a function in JavaScript ?",
+        options: [
+            { answer: "function myFunction()", isCorrect: true },
+            { answer: "var myFunction = function ()" },
+            { answer: "myFunction()" },
+            { answer: "new function myFunction()" },
+
+        ]
+    },
+    {
+        number: 8,
+        question: " Which of the following is not a conditional statement in JavaScript ?",
+        options: [
+            { answer: "if" },
+            { answer: "else" },
+            { answer: "switch" },
+            { answer: "then", isCorrect: true },
+
+        ]
+    },
+    {
+        number: 9,
+        question: "What is the correct way to access an element by its id in JavaScript ?",
+        options: [
+            { answer: 'getElementById("myId")', isCorrect:true },
+            { answer: 'findElement("myId")' },
+            { answer: "document.myId" },
+            { answer: "#myId" },
+
+        ]
+    },
+    {
+        number: 10,
+        question: "What is the correct way to convert a string to an integer in JavaScript ?",
+        options: [
+            { answer: 'stringToInt("myString")' },
+            { answer: 'parseInt("myString")', isCorrect: true },
+            { answer: 'toInt("myString")' },
+            { answer: 'int("myString")' },
+
+        ]
+    },
+
+]
