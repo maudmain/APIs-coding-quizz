599b37aedda057236147c67cbe2ded81f44ab6c9
maudmain <117314424+maudmain@users.noreply.github.com>
2023-01-22 20:46:44 +0000

    comments added and syntax errors corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,20 @@ let questionContainer = document.querySelector("#questions");
 // declare variables needed
 let time;
 let timer;
-let timerCount;
 let questionCounter;
+let score;
 
 questionsDOM = [];
 
-// the startGame function is called when the start button is clicked
+/* the startGame function is called when the start button is clicked 
+- the first question will appear (call transition function)
+- timer starts (call setTimer function)
+*/
 function startGame() {
 
-    questionCounter = -1;
+    questionCounter = -1; // starts at -1 so the first question is index 0
 
+    // ?? 
     questionsArray.forEach(q => {
         questionsDOM.push({
             element: createQuestionDiv(q),
@@ -26,10 +30,13 @@ function startGame() {
         })
     })
 
+   
     setTimer();
     transition();
 };
 
+
+// function to create a div element for each question
 function createQuestionDiv(question) {
     let questionDiv = document.createElement("div");
     let questionTitle = document.createElement("h2");
@@ -63,7 +70,10 @@ function createQuestionDiv(question) {
     return questionDiv;
 
 }
-// when the user click an answer
+/* when the user click an answer
+- move to next question
+- remove time if incorrect answer
+*/
 function answerClick(event) {
     let currentQuestion = questionsDOM[questionCounter];
 
@@ -71,11 +81,12 @@ function answerClick(event) {
     if (event.target.dataset.isCorrect === 'true') {
         event.target.className = "correct";
         currentQuestion.answer = true;
+       
 
     } else {
         event.target.className = "incorrect";
         currentQuestion.answer = false;
-        time -= 10;
+        time -= 5;
 
 
     }
@@ -87,11 +98,11 @@ function setTimer() {
     time = questionsArray.length * 7;
     let timer = setInterval(function () {
         time--;
-        timerElement.textContent = time
+        timerElement.textContent = time;
 
         if (time <= 0) {
             clearInterval(timer);
-            timerElement.textContent = "Time Off. Game over"
+            timerElement.textContent = "Time Off. Game over";
             endGame();
         }
     }, 1000);
@@ -102,6 +113,7 @@ function setTimer() {
 startButton.addEventListener("click", () => { startGame() });
 
 // transition function
+// hide the current question once answer and move to the next one ()
 function transition() {
     if (questionCounter >= 0) {
         let currentQuestion = questionsDOM[questionCounter];
@@ -111,7 +123,7 @@ function transition() {
 
     if (questionCounter < questionsDOM.length) {
         let newQuestion = questionsDOM[questionCounter];
-        newQuestion.element.className = "question"
+        newQuestion.element.className = "question";
     } else {
         endGame();
     }
@@ -119,15 +131,20 @@ function transition() {
 
 // function endGame
 function endGame() {
-    let score = 0
+    score = 0;
     questionsDOM.forEach(q => {
-        q.element.className = "question"
+        q.element.className = "question";
         if (q.answer === true) {
             score++;
         }
         q.element.querySelectorAll("button").forEach(b => b.removeEventListener("click", answerClick) );
     });
-    alert("you're score is" + score);
+    alert("you're score is " + score);
 }
 
+// end-screen to be displayed instead of start-screen
+
+
+
+// scores to be stored localy and retrieve on the highscores page
 
